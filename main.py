f3b59bf32737d3e59724c8b29e52efdd70c8a3dd
Your Name <you@example.com>
2025-01-30 01:23:54 +0500

    fix

--- main.py
+++ main.py
@@ -214,9 +214,9 @@ def swap_faces_and_download(image1_path, image2_path, download_dir):
         driver.quit()
 
 # 🔹 Пример использования
-image1 = "test_photos/book_image.png"
-image2 = "test_photos/alihan.png"
-download_directory = "test_photos"
+image1 = "/test_photos/book_image.png"
+image2 = "/test_photos/alihan.png"
+download_directory = "/test_photos"
 
 downloaded_file = swap_faces_and_download(image1, image2, download_directory)
 print(f"📥 Загруженный файл: {downloaded_file}")
